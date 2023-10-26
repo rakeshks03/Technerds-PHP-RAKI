e7eb6945c39b5a8226b44597e1a82dc55850647b
rakesh ks <rakesh2003ks@gmail.com>
2023-10-26 16:24:31 +0530

    26-10-2023

---
+++ 26-10-2023/comments.php
@@ -0,0 +1,20 @@
+<?php
+$a="technerds";
+$name="hello word";
+$type=false;
+$age=14;
+echo $name;
+echo("$a<br>");
+echo("rakesh"."<br>");
+print"$a<br>";
+var_dump ($type);
+var_dump ($age);
+echo $a."<br>";
+var_dump ($a);
+print"technerds"."<br>".$a."<br>";
+echo "<b>";
+print"ndrk<br>".$name;
+echo"<br>";
+print"Technerds"."<br>";
+echo("technerds"."<br>".$name);
+?>
